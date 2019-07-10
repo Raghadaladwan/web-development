4eaf87a0dbbd54a8592c0528228780a33e77959b
Raghadaladwan <48241038+Raghadaladwan@users.noreply.github.com>
2019-07-10 17:37:33 +0300

    Update exersices.js

--- exercises/js/if-conditions/exersices.js
+++ exercises/js/if-conditions/exersices.js
@@ -1,7 +1,18 @@
 /* 1. Write a JavaScript program that accept two integers and
  * display the larger.Go to the editor */
 /******* Start Your Code *********/
-  
+  function towIntegers(num1,num2)
+{
+	if(num1> num2)
+	{
+		document.write("The larger num is : "+num1);
+	}
+	else
+	{
+		document.write("The larger num is : "+num2);
+	}
+
+}
  /******* End Your Code ********* */
 
 /* 2. Write a JavaScript conditional statement to find the sign
@@ -11,6 +22,18 @@ Sample numbers : 3, -7, 2
 Output : The sign is - */
 /******* Start Your Code *********/
   
+function towIntegers(num1)
+{
+	if(num1>0)
+	{
+		document.write("The sign is " + "+");
+	}
+	else
+	{
+		document.write("The sign is " + "-");
+	}
+
+}
  /******* End Your Code ********* */
 
 /* 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor 
@@ -27,18 +50,61 @@ Sample numbers : -5, -2, -6, 0, -1
 Output : 0 */
 
 /******* Start Your Code *********/
-  
+  function fun(n1,n2,n3,n4,n5)
+ {
+ 	min=0;
+   if (n1<n2 && n1<n3 && n1<n4 && n1<n5)
+{
+    return min=n1;
+}
+else if (n2<n1 && n2<n3 && n2<n4 && n2<n5)
+{
+    return min=n2;
+}
+else if (n3<n1 && n3<n2 && n3<n4 && n3<n5)
+{
+   return min=n3;
+}
+else if (n4<n1 && n4<n2 && n4<n3 && n4<n5)
+{
+   return min=n4;
+}
+else
+{
+    return min=n5;
+}
+}
  /******* End Your Code ********* */
 
  /* 5.Fix the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
 /******* Start Your Code *********/
-  
+
+function towIntegers(x,y)
+{
+	if(x>y)
+	{
+		return"Hello World";
+	}
+	else
+	{
+		return "Goodbye";
+	}
+	
+}
+
  /******* End Your Code ********* */
 
 
 /*write the if statement to display "Hello World" if x is greater than y.*/
 /******* Start Your Code *********/
-  
+  function towIntegers(x,y)
+{
+	if(x>y)
+	{
+		return"Hello World";
+	}
+}
+
  /******* End Your Code ********* */
 
 
